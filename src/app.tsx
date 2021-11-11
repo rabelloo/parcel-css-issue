fd04ebf642f5f9b31cca7a2864c552be76db1499
Andre Rabello <andre.rabello@onfido.com>
2021-11-11 11:24:22 +0000

    init

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import * as styles from './app.scss';
+
+export const App = () => (
+  <>
+    <p className={styles.red}>
+      <code>app.scss</code> CSS module <code>.red =&gt; {styles.red}</code>
+    </p>
+
+    <p className="green">
+      <code>global.scss</code> global <code>.green</code>
+    </p>
+  </>
+);
